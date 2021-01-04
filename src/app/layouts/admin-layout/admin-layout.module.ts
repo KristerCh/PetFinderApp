db543bf7569f42fb723caeb81ab0e28e8f7b59a6
KristerCh <KBMChavez@outlook.com>
2021-01-04 00:28:56 -0600

    edit form profile

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -17,6 +17,8 @@ import {MatRippleModule} from '@angular/material/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 
 @NgModule({
   imports: [
@@ -30,6 +32,7 @@ import {MatSelectModule} from '@angular/material/select';
     MatInputModule,
     MatSelectModule,
     MatTooltipModule,
+    MatCheckboxModule
   ],
   declarations: [
     DashboardComponent,
