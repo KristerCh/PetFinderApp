b834f00acddee59c8092ad3f4352dca65f6f34e6
KristerCh <KBMChavez@outlook.com>
2021-01-06 00:56:00 -0600

    create functions save and edit profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,7 +1,10 @@
+
+import { EntityService } from './../Services/entity.service';
 import { Entity } from './../Models/Entity';
 import { Subscription } from 'rxjs/Subscription';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+declare var $: any;
 
 @Component({
   selector: 'app-user-profile',
@@ -9,14 +12,53 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-  entityProfile: FormGroup;
+  profileForm: FormGroup;
   subscription: Subscription;
   profile: Entity;
-  idProfile = 0;
+  createMode: boolean = true;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+    private profileSevice: EntityService) { }
 
   ngOnInit() {
+    this.profileForm = this.formBuilder.group({
+      identification: ['', Validators.required],
+      userName: ['', Validators.required, Validators.pattern("[A-Za-z]")],
+      name: ['', Validators.pattern("[A-Za-z]")],
+      lastName: ['', Validators.pattern("[A-Za-z]")],
+      phoneNumber: ['',Validators.required, Validators.pattern("[0-9]")],
+      email: ['', Validators.required, Validators.email],
+      address: ['', Validators.required],
+      photo: ['', Validators.required],
+      whatsapp: true
+    })
+    if(!this.createMode){
+      this.loadForm(this.profile);
+    }
+  }
+
+  loadForm(values){
+    this.profileForm.patchValue(values);
+  }
+
+  saveProfile(){
+    if(this.profileForm.invalid){
+      return
+    }
+
+    if(this.createMode){
+      let profilEntity: Entity = this.profileForm.value; 
+      this.profileSevice.saveEntity(profilEntity).subscribe(data => {
+        $.notify({icon: "notifications", message: "Registered Profile!"});
+        this.profileForm.reset;
+      })
+    }else{
+      let profilEntity: Entity = this.profileForm.value;
+      profilEntity.idEntity = this.profile.idEntity;
+      this.profileSevice.editEntity(profilEntity.idEntity, profilEntity).subscribe(data => {
+        $.notify({icon: "notifications", message: "Updated Profile!"})
+      })
+    }
   }
 
 }
